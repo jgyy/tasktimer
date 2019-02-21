1204868ff8efee883c79be2f995939b730e9e221
jgyy <jgyy@github.com>
2019-02-21 17:04:52 +0800

    initial task timer commit

---
+++ app/src/main/java/com/timbuchalka/tasktimer/Timing.java
@@ -0,0 +1,14 @@
+package com.timbuchalka.tasktimer;
+
+import java.io.Serializable;
+
+/**
+ * Simple timing object.
+ * Sets it's start time when created, and calculates how long since creation,
+ * when setDuration is called.
+ */
+
+class Timing implements Serializable {
+    public static final long serialVersionUID = 20161120L;
+
+}
